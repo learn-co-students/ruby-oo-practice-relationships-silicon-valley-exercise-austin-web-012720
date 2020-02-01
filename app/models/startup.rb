f055ecd22d29f31804e248fe13d0d5627d75d9c9
github username <github email address>
2020-01-31 21:30:01 -0600

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,41 @@
 class Startup
+  attr_reader :founder
+  attr_accessor :name, :domain
 
+  @@all = []
+
+  def initialize(name, founder)
+    @name = name
+    @founder = founder
+    @domain = domain
+
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+    self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founders)
+    self.all.find do |startup|
+      startup.founder == founder
+    end
+  end
+
+  def self.domains
+    self.all.map do |startup|
+      startup.domain.uniq
+    end
+  end
+  
+  def self.sign_contract(vc, type, worth)
+    Fundinground.new(self, vc, type)
+  end  
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("Cool_tech", "Mark")
+startup2 = Startup.new("Dance_studio", "Sydney")
+startup3 = Startup.new("Space_robots", "Sophie")
+
+vc1 = VC.new("Jim", 100000)
+vc2 = VC.new("Jilynn", 10000000000)
+vc3 = VC.new("Matt", 1200000000000)
+vc4 = VC.new("Nancy", -130000)
+
+fund1 = FundingRound.new(startup1, vc2, "type1")
+fund2 = FundingRound.new(startup2, vc3, "type2")
+fund3 = FundingRound.new(startup3, vc4, "type3")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
